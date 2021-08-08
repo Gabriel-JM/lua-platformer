7d4097fc1b3775d3e351aac4ce8bce2d4cb1aca7
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-08-07 21:33:48 -0300

    Init project and set player physics

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple Platform Game
+
+A simple platform game made with lua and love2d.
+
+## Dependencies
+[windfield](https://github.com/a327ex/windfield), placed in modules/windfield in root folder
---
+++ main.lua
@@ -0,0 +1,17 @@
+local wf = require 'modules/windfield/windfield'
+
+function love.load()
+  world = wf.newWorld(0, 800)
+
+  player = world:newRectangleCollider(360, 100, 80, 80)
+  platform = world:newRectangleCollider(250, 400, 300, 100)
+  platform:setType('static')
+end
+
+function love.update(dt)
+  world:update(dt)
+end
+
+function love.draw()
+  world:draw()
+end
